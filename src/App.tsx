0df7cf496a9c37809b2f6a80b5fc81a10bc0da8a
Austin19111 <austin.tjokrohandoko@binus.ac.id>
2025-05-17 22:14:26 +0700

    Frontend-tambahan

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import motokoShadowLogo from './assets/motoko_shadow.png';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 
+
 function App() {
   const { data: count, refetch } = useQueryCall({
     functionName: 'get',
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,26 @@ import { ActorProvider, AgentProvider } from '@ic-reactor/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { canisterId, idlFactory } from './declarations/backend';
+// import { canisterId, idlFactory } from './declarations/backend';
 import './index.scss';
+import LandingPage from './LandingPage';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AgentProvider withProcessEnv>
-      <ActorProvider idlFactory={idlFactory} canisterId={canisterId}>
-        <App />
-      </ActorProvider>
-    </AgentProvider>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <LandingPage />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <AgentProvider withProcessEnv>
+//       {/* <ActorProvider idlFactory={idlFactory} canisterId={canisterId}> */}
+//         <LandingPage />
+//         {/* <App /> */}
+//       {/* </ActorProvider> */}
+//     </AgentProvider>
+//   </React.StrictMode>,
+// );
