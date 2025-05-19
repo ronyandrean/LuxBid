bc164071d5919a440d92da4f691e0d2af69d4da1
Austin19111 <austin.tjokrohandoko@binus.ac.id>
2025-05-19 09:00:41 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ import App from './App';
 
 // import { canisterId, idlFactory } from './declarations/backend';
 // import './index.scss';
-<<<<<<< HEAD
-import LandingPage from './LandingPage';
-import AboutUsPage from './AboutUs';
-import LuxuryCollection from './CollectionLuxury';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-=======
+
+// import LandingPage from './LandingPage';
+// import AboutUsPage from './AboutUs';
+import LuxuryCollection from './pages/CollectionLuxury';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 // import LandingPage from './LandingPage';
 // import AboutUsPage from './AboutUs';
 import { BrowserRouter } from 'react-router-dom';
->>>>>>> 938947ce5f0d5264256e2c389ea04a7e7b04e75e
+
 
 // import { canisterId, idlFactory } from './declarations/backend';
 import './index.css';
@@ -22,17 +22,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutUsPage />} />
-        <Route path="/collection/luxury" element={<LuxuryCollection />}></Route>
-      </Routes>
-    </BrowserRouter>
-=======
       <App />
->>>>>>> 938947ce5f0d5264256e2c389ea04a7e7b04e75e
   </React.StrictMode>
 );
 
